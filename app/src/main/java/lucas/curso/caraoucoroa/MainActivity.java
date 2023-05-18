9c581b8a043c071024ff95799bd4f55bace5d97c
Lucas <lucas_fernando@ufms.br>
2023-05-18 19:59:34 -0400

    segundo commit

--- app/src/main/java/lucas/curso/caraoucoroa/MainActivity.java
+++ app/src/main/java/lucas/curso/caraoucoroa/MainActivity.java
@@ -3,6 +3,7 @@ package lucas.curso.caraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private Button bt_jogar;
+    private MyReceiver receiver = new MyReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +34,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(in);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        this.registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
